fdc0461b1b7f0008ae5c5bc8debab0d3592cb5b1
Marsi Starchild <marsi.starchild@gmail.com>
2024-11-19 23:17:44 +0100

    Updating mp4-fix.bat
    Updating mp4-fix.bat
    	- Affected file count report

--- mp4-fix.bat
+++ mp4-fix.bat
@@ -3,10 +3,15 @@
 setlocal enabledelayedexpansion
 
 set directory=%1
+set /a count=0
 
 for %%F in (%directory%\*.mp4) do (
 	set "output=%directory%\%%~nF_out%%~xF"
 	ffmpeg -i %%F -vf "crop=in_w:in_h/2:0:0" -c:a copy !output!
 	del %%F
 	ren !output! %%~nxF
+	set /a count+=1
 )
+
+echo.  
+echo %count% fichier(s) traite(s).
